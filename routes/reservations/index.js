9c0dbbf3ced8307f8bdf5f0443e1d3b72bd0b934
dnnyp <dnnypham@gmail.com>
2020-02-19 16:14:17 -0500

    Add reservation if there are open tables
    - npm install moment
    - add Reservation method to count # of reservations at a given time
    - update post '/reservations' to check for available tables

--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -2,16 +2,40 @@ const express = require('express');
 const router = express.Router();
 const { Reservation } = require('../../models');
 
+const moment = require('moment');
+
 router.get('/', async (req,res) => {
   res.json(await Reservation.all());
 });
 
 router.post('/', async (req,res) => {
   const rsvp = req.body.rsvp;
-  const slot = new Date(rsvp.slot);
 
-  if(slot.getUTCHours() < 3 || slot.getUTCHours() >= 18) {
-    res.json(await Reservation.add(rsvp));
+  // the requested slot and the slots 30 mins before and 30 mins after
+  const requestedSlot = moment(rsvp.slot);
+  const beforeSlot = requestedSlot.clone().subtract(30, 'm');
+  const afterSlot = requestedSlot.clone().add(30, 'm');
+
+  // if requested slot is between 1pm and 10pm
+  if(requestedSlot.hours() > 1 && requestedSlot.hours() <= 22) {
+    // count of reservations at the requested slot, and 30 mins before and after
+    const requestedCount = await Reservation.count(requestedSlot);
+    const beforeCount = await Reservation.count(beforeSlot);
+    const afterCount = await Reservation.count(afterSlot);
+
+    // total count of reservations at the first half of the requested slot
+    const firstHalf = beforeCount + requestedCount;
+    // total count of reservations at the second half of the requested slot
+    const secondHalf = requestedCount + afterCount;
+    // count of available tables at the requested slot
+    const available = firstHalf > secondHalf ? 10 - firstHalf : 10 - secondHalf;
+
+    // add a new reservation if there are tables available
+    if(available > 0) {
+      res.json(await Reservation.add(rsvp));
+    } else {
+      res.sendStatus(418);
+    }
   } else {
     res.sendStatus(418);
   }
