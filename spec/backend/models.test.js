7848f71c29225bd8cebaf5079290e250f5b6e9ea
dnnyp <dnnypham@gmail.com>
2020-02-22 14:16:47 -0500

    Add another failing test case

--- spec/backend/models.test.js
+++ spec/backend/models.test.js
@@ -10,3 +10,8 @@ test("Reservation.all() should return an array", async () =>{
   const result = await Reservation.all() ||  [];
   expect(result.length).toEqual(24);
 });
+
+test("Reservation.all() should return an array", async () =>{
+  const result = await Reservation.all() ||  [];
+  expect(result.length).toEqual(23);
+});
